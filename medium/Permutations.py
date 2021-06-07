11ea437fcce4d327092724d3a9e669087eb2214c
Bhagirath_3301 <sarvaiya.1@iitj.ac.in>
2021-06-08 00:30:50 +0530

    Create Permutations.py

---
+++ medium/Permutations.py
@@ -0,0 +1,16 @@
+#question link : https://leetcode.com/problems/permutations/
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def dfs(nums, path, res):
+            if not nums:
+                res.append(path)
+            for i in range(len(nums)):
+                dfs(nums[:i]+nums[i+1:], path+[nums[i]], res)
+
+        dfs(nums, [], res)
+        return res
+
+    
