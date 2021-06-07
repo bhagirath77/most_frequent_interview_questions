f01b09ea441b2dcee108a9317b32c9ae489b4f37
Bhagirath_3301 <sarvaiya.1@iitj.ac.in>
2021-06-08 00:39:26 +0530

    Create Subsets.py

---
+++ medium/Subsets.py
@@ -0,0 +1,13 @@
+#question link: https://leetcode.com/problems/subsets/
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        self.backtracking(res,0,[],nums)
+        return res
+    def backtracking(self,res,start,subset,nums):
+        res.append(list(subset))
+        for i in range(start,len(nums)):
+            subset.append(nums[i])
+            self.backtracking(res,i+1,subset,nums)
+            subset.pop()
