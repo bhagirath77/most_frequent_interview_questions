bdd360740cadead5d691fc692110eb4273e62e26
Bhagirath_3301 <sarvaiya.1@iitj.ac.in>
2021-06-12 11:04:47 +0530

    Create Two Sum.py

---
+++ easy/Two Sum.py
@@ -0,0 +1,11 @@
+# question link: https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        h = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            if n not in h:
+                h[num] = i
+            else:
+                return [h[n], i]
