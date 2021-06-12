b9efa43ca8b439c7e264c390e18be5d5d965a196
Bhagirath_3301 <sarvaiya.1@iitj.ac.in>
2021-06-12 11:08:57 +0530

    Create Letter Combinations of a Phone Number.py

---
+++ medium/Letter Combinations of a Phone Number.py
@@ -0,0 +1,17 @@
+# question link : https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+
+class Solution(object):
+    def letterCombinations(self, digits):
+        if not digits:
+            return []
+        m = {"2":"abc", '3':"def", '4':"ghi", '5':"jkl", '6':"mno", '7':"pqrs", '8':"tuv", '9':"wxyz"}
+        ret = []
+        self.dfs(m, digits, "", ret)
+        return ret
+    
+    def dfs(self, m, digits, path, ret):
+        if not digits:
+            ret.append(path)
+            return 
+        for c in m[digits[0]]:
+            self.dfs(m, digits[1:], path+c, ret)
